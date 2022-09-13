e0f0ea3f11e0430745555a21b8ce10894f0ec43e
qospwmf <qospwmf@gmail.com>
2022-09-14 03:03:24 +0900

    [Back]⚡add : Spring Security 위에 OAuth2 + JWT 적용 - StompHandler 수정(JwtTokenProvider 이용) - Member 필드 추가(AuthProvider, refreshToken) - JwtAuthenticationFilter 수정(JwtTokenProvider 이용) - MemberRepository 수정(jpql 추후 QueryDsl로?) - SecurityConfig 수정(OAuth2 맞게 수정) - AuthService 수정(refreshToken으로 access토큰 갱신)

--- backend/src/main/java/com/ssafy/rideus/config/security/service/AuthService.java
+++ backend/src/main/java/com/ssafy/rideus/config/security/service/AuthService.java
@@ -1,9 +1,14 @@
 package com.ssafy.rideus.config.security.service;
 
 import com.ssafy.rideus.common.exception.NotFoundException;
+import com.ssafy.rideus.config.security.auth.CustomUserDetails;
+import com.ssafy.rideus.config.security.util.CookieUtil;
+import com.ssafy.rideus.config.security.util.JwtTokenProvider;
 import com.ssafy.rideus.domain.Member;
 import com.ssafy.rideus.repository.jpa.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -11,6 +16,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +29,11 @@ import static com.ssafy.rideus.common.exception.NotFoundException.USER_NOT_FOUND
 @RequiredArgsConstructor
 public class AuthService implements UserDetailsService {
 
+    @Value("${token.refresh-cookie-key}")
+    private String cookieKey;
+
     private final MemberRepository memberRepository;
+    private final JwtTokenProvider tokenProvider;
 
     @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
@@ -35,4 +47,34 @@ public class AuthService implements UserDetailsService {
         loginUserDetails.setMember(findMember);
         return loginUserDetails;
     }
+
+    public String refreshToken(HttpServletRequest request, HttpServletResponse response, String oldAccessToken) {
+        // 1. Validation Refresh Token
+        String oldRefreshToken = CookieUtil.getCookie(request, cookieKey)
+                .map(Cookie::getValue).orElseThrow(() -> new RuntimeException("no Refresh Token Cookie"));
+
+        if (!tokenProvider.validateToken(oldRefreshToken)) {
+            throw new RuntimeException("Not Validated Refresh Token");
+        }
+
+        // 2. 유저정보 얻기
+        Authentication authentication = tokenProvider.getAuthentication(oldAccessToken);
+        CustomUserDetails user = (CustomUserDetails) authentication.getPrincipal();
+
+        Long id = user.getId();
+
+        // 3. Match Refresh Token
+        String savedToken = memberRepository.getRefreshTokenById(id);
+
+        if (!savedToken.equals(oldRefreshToken)) {
+            throw new RuntimeException("Not Matched Refresh Token");
+        }
+
+        // 4. JWT 갱신
+        String accessToken = tokenProvider.createAccessToken(authentication);
+        tokenProvider.createRefreshToken(authentication, response);
+
+        return accessToken;
+    }
+
 }
