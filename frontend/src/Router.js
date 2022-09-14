de787b7dbf748382cee8c8dea3b26570ff16acc4
문석희 <zazachucky@naver.com>
2022-09-14 11:50:34 +0900

    [Front]⚡add :  카카오맵, GPS 테스트 코드 추가

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
+import { GpsTest } from "./pages/GpsTest";
+import MapTest from "./pages/MapTest";
 
 const Layout = () => {
   return (
@@ -14,7 +16,10 @@ export const Router = () => {
   return (
     <Routes>
       {/* 로고, 푸터, 내브바 */}
-      <Route path="/" element={<Layout />}></Route>
+      <Route path="/" element={<Layout />}>
+        {/* <Route index element={<MapTest />}></Route> */}
+        <Route index element={<GpsTest />}></Route>
+      </Route>
     </Routes>
   );
 };
