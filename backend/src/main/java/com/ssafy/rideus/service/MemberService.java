85bf5368775a7ba4ce710a157b8434fe00be9963
qospwmf <qospwmf@gmail.com>
2022-09-15 02:42:14 +0900

    [Back]⚡add : 로그인한 유저 정보 받기 - 여기서 닉네임, 이름 등 값이 비어있으면 추가정보를 받게 해야하나? 고민

--- backend/src/main/java/com/ssafy/rideus/service/MemberService.java
+++ backend/src/main/java/com/ssafy/rideus/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ssafy.rideus.service;
 import com.ssafy.rideus.common.exception.NotFoundException;
 import com.ssafy.rideus.domain.Member;
 import com.ssafy.rideus.dto.member.request.MemberMoreInfoReq;
+import com.ssafy.rideus.dto.member.response.MemberMeRes;
 import com.ssafy.rideus.repository.jpa.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,13 +19,25 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     public void updateMoreInformation(MemberMoreInfoReq memberMoreInfoReq, long memberId) {
-        Member findMember = memberRepository.findById(memberId)
-                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+        Member findMember = findById(memberId);
         findMember.updateMoreInfo(memberMoreInfoReq);
         memberRepository.save(findMember);
     }
 
+    @Transactional(readOnly = true)
     public boolean checkNicknameDuplicate(String nickname) {
         return memberRepository.existsByNickname(nickname);
     }
+
+    @Transactional(readOnly = true)
+    public Member findById(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
+    }
+
+    public MemberMeRes findByLoginMember(Long memberId) {
+        Member findMember = findById(memberId);
+        return MemberMeRes.of(findMember);
+    }
+
 }
