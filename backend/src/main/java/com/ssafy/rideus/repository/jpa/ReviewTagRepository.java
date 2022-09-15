6c949d6813291752646001f0f366cbd773361a58
고유라 <koyura183982@naver.com>
2022-09-15 14:51:02 +0900

    [Back] ⚡add : 리뷰 추가

--- backend/src/main/java/com/ssafy/rideus/repository/jpa/ReviewTagRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/jpa/ReviewTagRepository.java
@@ -1,7 +1,12 @@
 package com.ssafy.rideus.repository.jpa;
 
+import com.ssafy.rideus.domain.Review;
 import com.ssafy.rideus.domain.ReviewTag;
+import com.ssafy.rideus.domain.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewTagRepository extends JpaRepository<ReviewTag, Long> {
+    List<Tag> findReviewTagsByReview(Review review);
 }
