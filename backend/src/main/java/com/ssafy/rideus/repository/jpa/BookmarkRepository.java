0288a2d1204b9c8c09b4227f8652f08ae6fd6aa1
zazachucky <zazachucky@naver.com>
2022-09-15 17:29:01 +0900

    [Back]🔄update : 북마크, 최근 주행 기록 DAO 추가

--- backend/src/main/java/com/ssafy/rideus/repository/jpa/BookmarkRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/jpa/BookmarkRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.rideus.repository.jpa;
 import com.ssafy.rideus.domain.Bookmark;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
+    public List<Bookmark> findTop5ByMemberIdOOrderByIdDesc(Long id);
 }
