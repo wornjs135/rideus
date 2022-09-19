435a3f967492ea77f21aff0ad3f3ad1b4f2b938e
qospwmf <qospwmf@gmail.com>
2022-09-19 17:07:40 +0900

    [Front]⚡add : 로그인 화면 및 로그인 추가 - Redirect 처리 필요

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -1,36 +1,49 @@
 import React from "react";
-import { Outlet, Route, Routes, useNavigate } from "react-router-dom";
-import { Footer, LogoHeader, NavBar } from "./components/Common.jsx";
-import { Course } from "./pages/Course";
-import { Main } from "./pages/Main";
-import { MyPage } from "./pages/MyPage";
-import { Rank } from "./pages/Rank";
-import { GpsTest } from "./pages/test/GpsTest";
+import {Outlet, Route, Routes, useNavigate} from "react-router-dom";
+import {Footer, LogoHeader, NavBar} from "./components/Common.jsx";
+import {Course} from "./pages/Course";
+import {Main} from "./pages/Main";
+import {MyPage} from "./pages/MyPage";
+import {Rank} from "./pages/Rank";
+import {GpsTest} from "./pages/test/GpsTest";
+import {Login} from "./pages/Login"
 import MapTest from "./pages/test/MapTest";
 
 const Layout = () => {
-  return (
-    <div>
-      <LogoHeader />
-      <Outlet />
-      <Footer />
-      <NavBar />
-    </div>
-  );
+    return (
+        <div>
+            <LogoHeader/>
+            <Outlet/>
+            <Footer/>
+            <NavBar/>
+        </div>
+    );
 };
 
+const WithOutHeaderNav = () => {
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    )
+}
+
 export const Router = () => {
-  return (
-    <Routes>
-      {/* 로고, 푸터, 내브바 */}
-      <Route path="/" element={<Layout />}>
-        {/* <Route index element={<MapTest />}></Route> */}
-        <Route index element={<Main />} />
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="/course" element={<Course />} />
-        <Route path="/rank" element={<Rank />} />
-        <Route path="/gpsTest" element={<GpsTest />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            {/* 로고, 푸터, 내브바 */}
+            <Route path="/" element={<Layout/>}>
+                {/* <Route index element={<MapTest />}></Route> */}
+                <Route index element={<Main/>}/>
+                <Route path="/mypage" element={<MyPage/>}/>
+                <Route path="/course" element={<Course/>}/>
+                <Route path="/rank" element={<Rank/>}/>
+                <Route path="/gpsTest" element={<GpsTest/>}/>
+            </Route>
+            {/* 헤더, 내브바 없는 곳*/}
+            <Route element={<WithOutHeaderNav/>}>
+                <Route path="/login" element={<Login/>}/>
+            </Route>
+        </Routes>
+    );
 };
