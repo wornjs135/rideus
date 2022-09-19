54b50552105865ce31830da49febf6a16e64af3d
문석희 <zazachucky@naver.com>
2022-09-19 15:15:31 +0900

    [Front]⚡add : api 폴더, 기본 api 추가

---
+++ frontend/src/utils/api/testApi.js
@@ -0,0 +1,10 @@
+import { API_SERVER, axios } from "./api";
+
+const API_SERVER_USER = API_SERVER + "user/";
+
+const instance = axios.create({
+  baseURL: API_SERVER_USER,
+  headers: {
+    contentType: "application/json",
+  },
+});
