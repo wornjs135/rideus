0b2b681da2ccee10d400184a17dcbab05b6056ef
qospwmf <qospwmf@gmail.com>
2022-09-20 20:27:46 +0900

    [Front]⚡add : axios 인터셉터 추가 및 추가 정보입력

--- frontend/src/pages/login/MoreInfo.jsx
+++ frontend/src/pages/login/MoreInfo.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, {useState} from "react";
 import {Box} from "grommet";
-import {TextField} from "@mui/material";
-import Button from "../../components/Button";
+import {Button, TextField} from "@mui/material";
 
 
 export const MoreInfo = () => {
+    let [inputs, setInputs] = useState({
+        name: '',
+        phone: '',
+        nickname: '',
+    });
+
+    const {name, phone, nickname} = inputs;
+
+    const onChange = (e) => {
+        const {value, name} = e.target; // 우선 e.target 에서 name 과 value 를 추출
+        setInputs({
+            ...inputs, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
+        });
+    };
+
+    const onClick = (e) => {
+    }
+
     return <Box width="100vw" margin="0 auto" gap="small" height={"100vh"} align={"center"}>
         <Box align="center">
             <Box>추가 정보 입력</Box>
@@ -12,21 +30,24 @@ export const MoreInfo = () => {
 
         <Box>
             <Box>
-                <TextField id="filled-basic" label="이름" variant="standard" helperText=""/>
+                <TextField id="filled-basic" name="name" label="이름" variant="standard" helperText="" onChange={onChange}
+                           value={name}/>
             </Box>
             <Box>
-                <TextField id="filled-basic" label="E-mail" variant="standard" helperText=""/>
+                <TextField id="filled-basic" label="E-mail" variant="standard" helperText="" disabled/>
             </Box>
             <Box>
-                <TextField id="filled-basic" label="전화번호" variant="standard" helperText=""/>
+                <TextField id="filled-basic" name="phone" label="전화번호" variant="standard" helperText=""
+                           onChange={onChange} value={phone}/>
             </Box>
             <Box>
-                <TextField id="filled-basic" label="닉네임" variant="standard" helperText=""/>
+                <TextField id="filled-basic" name="nickname" label="닉네임" variant="standard" helperText=""
+                           onChange={onChange} value={nickname}/>
             </Box>
         </Box>
 
         <Box>
-            <Button BigGreen children="완료"/>
+            <Button variant="contained" style={{width : "70vw", background : "#439652"}} onClick={onClick}>확인</Button>
         </Box>
     </Box>;
 };
