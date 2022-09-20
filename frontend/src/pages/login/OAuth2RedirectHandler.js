aaf2a068de9bd51f82a05d538d1fcaea0e1da78d
qospwmf <qospwmf@gmail.com>
2022-09-20 15:44:19 +0900

    [Front]⚡add : 추가정보 미입력시 리다이렉트 및 추가정보 화면(대충)

--- frontend/src/pages/login/OAuth2RedirectHandler.js
+++ frontend/src/pages/login/OAuth2RedirectHandler.js
@@ -7,10 +7,20 @@ export const OAuth2RedirectHandler = (props) => {
 
     const accessToken = params.get("accessToken");
     const refreshToken = params.get("refreshToken");
+    const register = params.get("register");
+
+
+    let isRegister = Boolean(register);
 
     localStorage.setItem("accessToken", accessToken);
     localStorage.setItem("refreshToken", refreshToken);
 
-    navigate("/");
+    // 회원가입후 추가 입력 안했으면
+    if (isRegister === true) {
+        window.location.href = '/moreinfo';
+    } else {
+        window.location.href = '/';
+    }
+
     return null;
 };
