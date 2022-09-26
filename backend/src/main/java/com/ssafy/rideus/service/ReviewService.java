221c8bcff6ccb6b123c0b17b35f9005dab0518c5
rjeowornjs2 <rjeowornjs@naver.com>
2022-09-26 17:24:46 +0900

    [Back]🔄update : Record에 그룹방, 나만의 주행 여부, 완주 여부, Review에 그룹방 컬럼 추가에 따른 업데이트

--- backend/src/main/java/com/ssafy/rideus/service/ReviewService.java
+++ backend/src/main/java/com/ssafy/rideus/service/ReviewService.java
@@ -2,6 +2,7 @@ package com.ssafy.rideus.service;
 
 import com.ssafy.rideus.domain.Review;
 import com.ssafy.rideus.dto.review.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -9,7 +10,7 @@ public interface ReviewService {
     //mid 회원 번호, cid 코스 식별자, rid 리뷰 식별자
 
     //리뷰 작성
-    Review writeReview(ReviewRequestDto reviewRequestDto, Long mid);
+    WriteReviewResponse writeReview(ReviewRequestDto reviewRequestDto, Long mid, MultipartFile image);
     //코스 별 리뷰 전체 목록
     List<Review> showAllReview(String cid);
     //리뷰 상세
