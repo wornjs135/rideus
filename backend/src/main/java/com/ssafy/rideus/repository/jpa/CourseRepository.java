d7327d525fbdc8f4451a80c729448816f67a530a
wornjs135 <rjeowornjs@naver.com>
2022-09-27 20:21:07 +0900

    [Back]🔄update : 태그기반 코스추천에서 북마크 정보 추가

--- backend/src/main/java/com/ssafy/rideus/repository/jpa/CourseRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/jpa/CourseRepository.java
@@ -26,12 +26,12 @@ public interface CourseRepository extends JpaRepository<Course, String> {
             "where cnt_rank <= 5;", nativeQuery = true)
     List<CourseReviewTagTop5DtoInterface> getCourseReviewTagTop5();
 
-    @Query(value = "select distinct c.course_id as courseId, c.course_name as courseName, c.distance as distance, c.start as start, c.finish as finish, c.expected_time as expectedTime, c.like_count as likeCount, a.tag_id as tagId,  t.tag_name as tagName, max(matchCnt) as max\n" +
+    @Query(value = "select distinct c.course_id courseId, c.course_name courseName, c.distance distance, c.start start, c.finish finish, c.expected_time expectedTime, c.like_count likeCount, a.tag_id tagId,  t.tag_name tagName, max(matchCnt) max, bookmark_id bookmarkId, c.image_url imageUrl, c.category\n" +
             "from (select course_id, tag_id, @eq_cnt \\:= IF(tag_id in (SELECT tag_id FROM member_tag\n" +
             "\t\t\t\t\t\t\t\t\t\t\twhere member_id = :memberId) && @current_course_id2 = course_id, @eq_cnt + 1, 1) as matchCnt,\n" +
             "\t\t@current_course_id2 \\:= course_id\n" +
             "\t\tfrom course_tag\n" +
-            "\t\tgroup by course_id, tag_id) a join course c on a.course_id = c.course_id join tag t on a.tag_id = t.tag_id\n" +
+            "\t\tgroup by course_id, tag_id) a join course c on a.course_id = c.course_id join tag t on a.tag_id = t.tag_id left outer join (SELECT course_id, bookmark_id FROM bookmark WHERE member_id = :memberId) b on c.course_id = b.course_id\n" +
             "group by a.course_id, a.tag_id\n" +
             "order by max desc", nativeQuery = true)
     List<RecommendationCourseDtoInterface> getRecommendationCourseByMemberId(@Param("memberId") Long memberId);
