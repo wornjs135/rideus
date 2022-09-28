33015366ca09c2f0265d6a7dac46a9cd7acb47dc
고유라 <koyura183982@naver.com>
2022-09-28 14:16:39 +0900

    [Hadoop]♻️refactor : MongoDB 저장 코드 리펙토링

--- backend/src/main/java/com/ssafy/rideus/repository/mongo/CourseCoordinateRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/mongo/CourseCoordinateRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import com.ssafy.rideus.domain.collection.CourseCoordinate;
 
 public interface CourseCoordinateRepository extends MongoRepository<CourseCoordinate, String> {
+
 }
--- backend/src/main/java/com/ssafy/rideus/repository/mongo/NearInfoRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/mongo/NearInfoRepository.java
@@ -3,5 +3,7 @@ package com.ssafy.rideus.repository.mongo;
 import com.ssafy.rideus.domain.collection.NearInfo;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface NearInfoRepository extends MongoRepository<NearInfo, String> {
 }
