ed294e904f3b9cad518790a47f65b56a18d4f713
rjeowornjs2 <rjeowornjs@naver.com>
2022-10-01 14:42:49 +0900

    [Back]🔄update : 추천 코스 쿼리 수정

--- backend/src/main/java/com/ssafy/rideus/repository/jpa/CourseRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/jpa/CourseRepository.java
@@ -30,14 +30,17 @@ public interface CourseRepository extends JpaRepository<Course, String> {
             "where cnt_rank <= 5;", nativeQuery = true)
     List<CourseReviewTagTop5DtoInterface> getCourseReviewTagTop5();
 
-    @Query(value = "select distinct c.course_id courseId, c.course_name courseName, c.distance distance, c.start start, c.finish finish, c.expected_time expectedTime, c.like_count likeCount, a.tag_id tagId,  t.tag_name tagName, max(matchCnt) max, bookmark_id bookmarkId, c.image_url imageUrl, c.category\n" +
-            "from (select course_id, tag_id, @eq_cnt \\:= IF(tag_id in (SELECT tag_id FROM member_tag\n" +
-            "\t\t\t\t\t\t\t\t\t\t\twhere member_id = :memberId) && @current_course_id2 = course_id, @eq_cnt + 1, 1) as matchCnt,\n" +
-            "\t\t@current_course_id2 \\:= course_id\n" +
-            "\t\tfrom course_tag\n" +
-            "\t\tgroup by course_id, tag_id) a join course c on a.course_id = c.course_id join tag t on a.tag_id = t.tag_id left outer join (SELECT course_id, bookmark_id FROM bookmark WHERE member_id = :memberId) b on c.course_id = b.course_id\n" +
-            "group by a.course_id, a.tag_id\n" +
-            "order by max desc", nativeQuery = true)
+    @Query(value = "select * from            \n" +
+            "(select distinct c.course_id courseId, c.course_name courseName, c.distance distance, c.start start, c.finish finish,\n" +
+            "\t\t\tc.expected_time expectedTime, c.like_count likeCount, a.tag_id tagId,  t.tag_name tagName, max(matchCnt) max,  c.image_url imageUrl, c.category\n" +
+            "            from (select course_id, tag_id, @eq_cnt \\:= IF(tag_id in (SELECT tag_id FROM member_tag\n" +
+            "            where member_id = :memberId) && @current_course_id2 = course_id, @eq_cnt + 1, 1) as matchCnt,\n" +
+            "            @current_course_id2 \\:= course_id\n" +
+            "            from course_tag\n" +
+            "            group by course_id, tag_id\n" +
+            "            ) a join course c on a.course_id = c.course_id join tag t on a.tag_id = t.tag_id \n" +
+            "            group by a.course_id, a.tag_id\n" +
+            "            order by c.course_id asc, max desc) aa left join (SELECT course_id, bookmark_id bookmarkId FROM bookmark WHERE member_id = :memberId) bb on aa.courseId = bb.course_id;", nativeQuery = true)
     List<RecommendationCourseDtoInterface> getRecommendationCourseByMemberId(@Param("memberId") Long memberId);
 
     @Query(value = "select distinct c.course_id courseId, c.course_name courseName, c.distance distance, c.expected_time expectedTime,\n" +
