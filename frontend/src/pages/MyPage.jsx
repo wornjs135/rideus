b6b02182e69e68bfb91a6d81eb26285a339358d2
qospwmf <qospwmf@gmail.com>
2022-10-01 20:52:12 +0900

    [Front]⚡add : 마이페이지 기본 연결 및 css 변경

--- frontend/src/pages/MyPage.jsx
+++ frontend/src/pages/MyPage.jsx
@@ -1,15 +1,15 @@
 import React, {useEffect, useState} from "react";
 import {Box, Spinner} from "grommet";
 import bike1 from "../assets/images/bicycle.png";
-import weather from "../assets/images/weather.png";
 import {StyledText} from "../components/Common";
 import {StyledHorizonTable} from "../components/HorizontalScrollBox";
 import {MyPageCourse} from "../components/MyPageCourseComponent";
 import {UserSettings} from "grommet-icons";
-import {myRides, recentRide} from "../utils/api/userApi";
+import {getMyTag, getTotalRecord, myRides, recentRide} from "../utils/api/userApi";
 import {useSelector} from 'react-redux'
 import {useNavigate} from "react-router-dom";
 import {getBookmarkedCourses} from "../utils/api/bookmarkApi";
+import {convertDistanceToImg, convertDistanceToText, expectTimeHandle} from "../utils/util";
 
 export const MyPage = () => {
 
@@ -17,6 +17,8 @@ export const MyPage = () => {
     const [recentRides, setRecentRides] = useState([]);
     const [myRideCourses, setMyRideCourses] = useState([]);
     const [bookmarkCourses, setbookmarkCourses] = useState([]);
+    const [myTag, setMyTag] = useState([]);
+    const [totalRecord, setTotalRecord] = useState([]);
     const [loading, setLoading] = useState(true);
     const user = useSelector(state => state.user.user.user);
     useEffect(() => {
@@ -41,6 +43,17 @@ export const MyPage = () => {
             setbookmarkCourses(rides);
         }, () => {
         });
+        getMyTag((data) => {
+            console.log(data);
+            const {data: tags} = data;
+            setMyTag(tags);
+        })
+        getTotalRecord((data) => {
+            console.log(data);
+            const {data: record} = data;
+            setTotalRecord(record);
+        })
+
         console.log(user);
         setLoading(false);
     }, []);
@@ -51,29 +64,30 @@ export const MyPage = () => {
 
             <Box align="center" justify="between">
                 <Box direction="row" align="center" justify="around" style={{marginTop: "5vw"}}>
-                    <Box direction={"column"}>
+                    <Box direction={"column"} align={"left"} justify={"start"}>
                         <UserSettings style={{marginBottom: "5vw"}} onClick={() => navigate("/profile")}/>
                         <img src={bike1}/>
                     </Box>
                     <Box>
                         <Box direction="row" style={{marginBottom: "5vw", marginLeft: "auto"}}>
                             <Box style={{marginRight: "5vw"}}>
-                                <StyledText text="42h 27m" weight="bold" size="15px"
+                                <StyledText text={expectTimeHandle(totalRecord.total_time)} weight="bold" size="15px"
                                             style={{textAlign: "right", display: "inline"}}/>
                                 <StyledText
                                     color="#4B4B4B"
-                                    text="675km"
+                                    text={`${totalRecord.total_distance}km`}
                                     weight="bold"
                                     size="15px"
                                     style={{textAlign: "right", display: "inline"}}
                                 />
                             </Box>
-                            <img src={weather} width={50}/>
+                            <img src={convertDistanceToImg(totalRecord.total_distance)} width={50}/>
                         </Box>
                         <Box justify={"end"}>
-                            <StyledText text="SSAFY KIM님은 현재" weight="bold" size="18px"
+                            <StyledText text={`${user.nickname}님은 현재`} weight="bold" size="18px"
                                         style={{textAlign: "right", display: "inline"}}></StyledText>
-                            <StyledText text="서울에서 부산까지의 거리를" weight="bold" size="18px"
+                            <StyledText text={convertDistanceToText(totalRecord.total_distance)} weight="bold"
+                                        size="18px"
                                         style={{textAlign: "right", display: "inline"}}></StyledText>
                             <StyledText text="왕복했어요!" weight="bold" size="18px"
                                         style={{textAlign: "right", display: "inline"}}></StyledText>
@@ -152,25 +166,24 @@ export const MyPage = () => {
                         <StyledText text="MY RIDE" weight="bold" size="18px"/>
                     </Box>
                     {/* 인기태그 리스트 */}
-                    {/* 월간자전거 리스트 */}
-                    <Box direction="row" overflow="scroll">
-                        <StyledHorizonTable>
-                            {/*<div className="card">*/}
-                            {/*    <MyPageCourse/>*/}
-                            {/*</div>*/}
-                            {/*<div className="card">*/}
-                            {/*    <MyPageCourse/>*/}
-                            {/*</div>*/}
-                            {/*<div className="card">*/}
-                            {/*    <MyPageCourse/>*/}
-                            {/*</div>*/}
-                            {/*<div className="card">*/}
-                            {/*    <MyPageCourse/>*/}
-                            {/*</div>*/}
-                            {/*<div className="card">*/}
-                            {/*    <MyPageCourse/>*/}
-                            {/*</div>*/}
-                        </StyledHorizonTable>
+                    <Box
+                        direction="row"
+                        overflow="scroll"
+                        margin="medium"
+                        pad={{left: "20px"}}
+                    >
+                        {myTag &&
+                            myTag.map((tag, idx) => {
+                                return (
+                                    <StyledText
+                                        text={`#${tag.tagName}`}
+                                        key={tag.tagId}
+                                        color="#64CCBE"
+                                        weight="bold"
+                                        style={{marginRight: "10px"}}
+                                    />
+                                );
+                            })}
                     </Box>
                 </Box>
             </Box>
