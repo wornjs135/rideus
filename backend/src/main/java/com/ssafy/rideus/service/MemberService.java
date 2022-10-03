f8003a3def602dab53ffcaa66761af6659011721
qospwmf <qospwmf@gmail.com>
2022-10-03 17:38:52 +0900

    [Back]🔄update : 최근 주행, 나만의 주행 latlng 추가

--- backend/src/main/java/com/ssafy/rideus/service/MemberService.java
+++ backend/src/main/java/com/ssafy/rideus/service/MemberService.java
@@ -3,6 +3,7 @@ package com.ssafy.rideus.service;
 import com.ssafy.rideus.common.exception.NotFoundException;
 import com.ssafy.rideus.domain.Member;
 import com.ssafy.rideus.domain.Record;
+import com.ssafy.rideus.domain.collection.MongoRecord;
 import com.ssafy.rideus.dto.member.request.MemberMoreInfoReq;
 import com.ssafy.rideus.dto.member.response.MemberMeRes;
 import com.ssafy.rideus.dto.record.response.MyRideRecordRes;
@@ -10,6 +11,7 @@ import com.ssafy.rideus.dto.record.response.RecordTotalResponse;
 import com.ssafy.rideus.dto.tag.response.MemberTagResponse;
 import com.ssafy.rideus.repository.jpa.MemberRepository;
 import com.ssafy.rideus.repository.jpa.RecordRepository;
+import com.ssafy.rideus.repository.mongo.MongoRecordRepository;
 import com.ssafy.rideus.repository.query.MemberQueryRepository;
 import com.ssafy.rideus.repository.query.TagQueryRepository;
 import lombok.RequiredArgsConstructor;
@@ -21,6 +23,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.ssafy.rideus.common.exception.NotFoundException.RECORD_NOT_FOUND;
 import static com.ssafy.rideus.common.exception.NotFoundException.USER_NOT_FOUND;
 
 @Service
@@ -31,9 +34,11 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final MemberQueryRepository memberQueryRepository;
-    private  final TagQueryRepository tagQueryRepository;
+    private final TagQueryRepository tagQueryRepository;
+    private final MongoRecordRepository mongoRecordRepository;
 
     private final RecordRepository recordRepository;
+
     public void updateMoreInformation(MemberMoreInfoReq memberMoreInfoReq, long memberId) {
         Member findMember = findById(memberId);
         findMember.updateMoreInfo(memberMoreInfoReq);
@@ -60,6 +65,7 @@ public class MemberService {
     public void deleteMember(Long memberId) {
         memberRepository.deleteById(memberId);
     }
+
     @Transactional
     public void updateMember(MemberUpdateRequest request, Long memberId) {
         Member findMember = validateMember(memberId);
@@ -72,6 +78,7 @@ public class MemberService {
                 .orElseThrow(() -> new NotFoundException(USER_NOT_FOUND));
         return findMember;
     }
+
     @Transactional(readOnly = true)
     public RecordTotalResponse getTotalRecord(Long memberId) {
         return memberQueryRepository.searchTotalRecord(memberId);
@@ -90,8 +97,13 @@ public class MemberService {
             if (record.getCourse() != null) {
                 myRideRecordResList.add(MyRideRecordRes.sharedMyRide(record));
             } else {
+
+                MongoRecord mongoRecord = mongoRecordRepository.findById(record.getId())
+                        .orElseThrow(() -> new NotFoundException(RECORD_NOT_FOUND));
+                MyRideRecordRes myRideRecordRes = MyRideRecordRes.unSharedMyRide(record);
+                myRideRecordRes.setLatLng(mongoRecord);
                 // 공유되지 않은 나만의 주행
-                myRideRecordResList.add(MyRideRecordRes.unSharedMyRide(record));
+                myRideRecordResList.add(myRideRecordRes);
             }
         });
 
@@ -107,8 +119,13 @@ public class MemberService {
             if (record.getCourse() != null) {
                 myRideRecordResList.add(MyRideRecordRes.sharedMyRide(record));
             } else {
-            // 공유되지 않은 나만의 주행
-                myRideRecordResList.add(MyRideRecordRes.unSharedMyRide(record));
+                MongoRecord mongoRecord = mongoRecordRepository.findById(record.getId())
+                        .orElseThrow(() -> new NotFoundException(RECORD_NOT_FOUND));
+
+                MyRideRecordRes myRideRecordRes = MyRideRecordRes.unSharedMyRide(record);
+                myRideRecordRes.setLatLng(mongoRecord);
+                // 공유되지 않은 나만의 주행
+                myRideRecordResList.add(myRideRecordRes);
             }
         });
 
