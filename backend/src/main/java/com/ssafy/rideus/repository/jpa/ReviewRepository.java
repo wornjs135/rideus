301502beef80e8dd09350159bab2919d2fb90bfc
rjeowornjs2 <rjeowornjs@naver.com>
2022-10-05 14:44:56 +0900

    [Back]🔄update : review 상세 조회 프로필, 기록좌표 추가

--- backend/src/main/java/com/ssafy/rideus/repository/jpa/ReviewRepository.java
+++ backend/src/main/java/com/ssafy/rideus/repository/jpa/ReviewRepository.java
@@ -2,9 +2,14 @@ package com.ssafy.rideus.repository.jpa;
 
 import com.ssafy.rideus.domain.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
     List<Review> findAllByCourseId(String id);
+
+    @Query("select r from Review r join fetch r.member join fetch r.record")
+    Optional<Review> findReviewWithMemberAndRecordById(Long reviewId);
 }
