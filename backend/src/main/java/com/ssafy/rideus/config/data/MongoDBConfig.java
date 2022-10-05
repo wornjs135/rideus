655ecfc0213ef6d64147fb76e0802ee4007b0eff
rjeowornjs2 <rjeowornjs@naver.com>
2022-10-05 11:21:54 +0900

    [Back]🛠️fix : 주행 후 좌표리스트 저장 수정

--- backend/src/main/java/com/ssafy/rideus/config/data/MongoDBConfig.java
+++ backend/src/main/java/com/ssafy/rideus/config/data/MongoDBConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
 import org.springframework.data.mongodb.core.convert.DbRefResolver;
@@ -44,5 +45,10 @@ public class MongoDBConfig {
         converter.setTypeMapper(new DefaultMongoTypeMapper(null));
 
         return converter;
+    }
+
+    @Bean
+    public MongoTransactionManager transactionManager(MongoDatabaseFactory dbFactory) {
+        return new MongoTransactionManager(dbFactory);
     }
 }
